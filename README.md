5d4d2fc8e03ab00247dbba93126d58834fabfa01
Kevin Litzenberg <kevin.litzenberg@gmail.com>
2018-09-26 05:58:26 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# ubiquiti_cloud
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,20 @@
+name 'ubiquiti_cloud'
+maintainer 'The Authors'
+maintainer_email 'kevin.litzenberg@gmail.com'
+license 'All Rights Reserved'
+description 'Installs/Configures ubiquiti_cloud'
+long_description 'Installs/Configures ubiquiti_cloud'
+version '0.1.0'
+chef_version '>= 12.14' if respond_to?(:chef_version)
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/ubiquiti_cloud/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/ubiquiti_cloud'
